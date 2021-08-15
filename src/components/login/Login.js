cc023c5bd4dc4c6f34a4644482cb44fb25c349f9
Parahat <nepesovparahatt@gmail.com>
2021-08-15 13:06:06 +0300

    Updated login page css

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,47 +1,64 @@
-import React from 'react'
+import React from "react";
 import avatar from "../../assets/images/avatar.svg";
 import "./Login.scss";
 
 const Login = () => {
-    return (
-        <div class="login">
-           <div className="login__column">
-               <div className="login__avatar">
-               <img src={avatar} alt="avatar-logo"/>
-               </div>
-               <div class="row">
-            <div class="col-md-4 offset-md-4">
-                <div class="login-form bg-light mt-4 p-4">
-                    <form action="" method="" class="row g-3">
-                        <h4>Welcome Back</h4>
-                        <div class="col-12">
-                            <label>Username</label>
-                            <input type="text" name="username" class="form-control" placeholder="Username"/>
-                        </div>
-                        <div class="col-12">
-                            <label>Password</label>
-                            <input type="password" name="password" class="form-control" placeholder="Password"/>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="rememberMe"/>
-                                <label class="form-check-label" for="rememberMe"> Remember me</label>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-dark float-end">Login</button>
-                        </div>
-                    </form>
-                    <hr class="mt-4"/>
-                    <div class="col-12">
-                        <p class="text-center mb-0">Have not account yet? <a href="#">Signup</a></p>
-                    </div>
-                </div>
+  return (
+    <div class="d-flex justify-content-center login">
+      <div class="col-md-3">
+        <div class="login-form login__box-rounded bg-light m4-2 p-4 shadow-lg">
+          <div class="d-flex justify-content-center pb-4">
+            <img
+              src={avatar}
+              alt="avatar-logo"
+              class="img-fluid img-thumbnail"
+            />
+          </div>
+          <form class="row g-3">
+            <div class="col-12">
+              <input
+                id="email"
+                type="email"
+                class="form-control"
+                name="email"
+                placeholder="Email"
+                required
+                autofocus
+              />
+              <div class="invalid-feedback">Email is invalid</div>
             </div>
+            <div class="col-12">
+              <input
+                type="password"
+                name="password"
+                class="form-control"
+                placeholder="Password"
+                required
+                autofocus
+              />
+            </div>
+            <div class="col-12">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="rememberMe"
+                />
+                <label class="form-check-label" for="rememberMe">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <div class="d-grid gap-4 col-6 mx-auto">
+              <button type="submit" class="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-           </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
